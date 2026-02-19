0c0615cdcfb5bd8a8aff913d4f9751694f6a50c1
Aditya Dumbre <adityad.9051@gmail.com>
2026-02-19 22:39:53 +0530

    update

--- java/Logic Building/4/Pattern9.java
+++ java/Logic Building/4/Pattern9.java
@@ -8,6 +8,30 @@ class Pattern9{
 		
 		int size = sc.nextInt();
 
-		for(int i = 0 ; i < size ; i++
+		for(int i = 0 ; i < size ; i++){
+			for(int j = 0 ; j < size-i ; j++){
+				System.out.print(" ");
+			}
+			for(int j = 0 ; j <= 2*i ; j++){
+				System.out.print("*");
+			}
+			for(int j = 0 ; j < size-i ; j++){
+				System.out.print(" ");
+			}
+		System.out.println();			
+		}
+
+		for(int i = size ; i >= 0 ; i--){
+			for(int j = 0 ; j < size-i ; j++){
+				System.out.print(" ");
+			}
+			for(int j = 0 ; j <= 2*i ; j++){
+				System.out.print("*");
+			}
+			for(int j = 0 ; j < size-i ; j++){
+				System.out.print(" ");
+			}
+		System.out.println();			
+		}
 	}
 }
